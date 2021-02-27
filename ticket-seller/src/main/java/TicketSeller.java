05468341c074807de38c1928470bc92ff753b302
Junha Kim <kimjh4930@gmail.com>
2021-02-28 02:09:37 +0900

    ticket seller refactoring

--- ticket-seller/src/main/java/TicketSeller.java
+++ ticket-seller/src/main/java/TicketSeller.java
@@ -5,7 +5,11 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo (Audience audience){
+        ticketOffice.plusAmount(
+                audience.buy(
+                        ticketOffice.getTicket()
+                )
+        );
     }
 }
